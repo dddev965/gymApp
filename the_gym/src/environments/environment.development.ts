5011dea3c445fd53c962cd6b644e1e567989e3c5
dddev965 <dddev965@gmail.com>
2024-03-07 15:40:51 +0530

    environmentsfile

---
+++ the_gym/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    firebaseKey:'https://gym-app-46fea-default-rtdb.firebaseio.com/gym.json'
+
+};
---
+++ the_gym/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    firebaseKey:'https://gym-app-46fea-default-rtdb.firebaseio.com/gym.json'
+};
